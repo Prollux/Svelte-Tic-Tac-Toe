70400a2b6b200ba0744b7e3fb13460fb7470c9c7
Zach Shold <sholdzb@gmail.com>
2021-07-21 16:09:37 -0500

    renders button to screen that updates with current player if empty

---
+++ src/components/Tile.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let id
+  export let player
+  export let changePlayer
+  const updateTile = (e) => {
+    if (!e.srcElement.innerText) {
+      e.srcElement.innerText = player
+      changePlayer()
+    }
+  }
+</script>
+
+
+<main>
+  <button id={id} on:click={updateTile}></button>
+</main>
